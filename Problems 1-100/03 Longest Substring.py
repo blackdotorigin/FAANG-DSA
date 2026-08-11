5580058aa654a4f91413a404fc426cfba8725159
blackdotorigin <blackdotorigin@gmail.com>
2026-08-11 23:24:15 +0530

    Third Sum Commit

--- Problems 1-100/03 Longest Substring.py
+++ Problems 1-100/03 Longest Substring.py
@@ -18,3 +18,38 @@ Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
 '''
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        result = ""
+        for i in range(len(s)):
+            char = s[i]
+            char_set = set()
+            char_set.add(s[i])
+            for j in range(i+1,len(s)):
+                if s[j] not in char_set:
+                    char+= s[j]
+                    char_set.add(s[j])
+                else:
+                    break
+            result = char if len(char) > len(result) else result
+        return result
+
+    def optimal(self,s:str)->int:
+        result = ""
+        char_dict = dict()
+        i,j = 0,0
+        while j < len(s):
+            if s[j] in char_dict and char_dict[s[j]] >= i:
+                result = result if j-i < len(result) else s[i:j]
+                i = char_dict[s[j]] + 1
+                char_dict[s[j]] = j
+            else:
+                char_dict[s[j]] = j
+            j+=1
+        return result
+
+
+
+
+sol = Solution()
+print(sol.optimal("abcabcbb"))
